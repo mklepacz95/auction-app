520134612176527ad22c92472d4ff17af6aeb630
mklepacz95 <a@a.com>
2019-06-18 22:44:31 +0200

    fixup! Fix CORS bug

--- src/main/java/auctionapp/api/LoginApi.java
+++ src/main/java/auctionapp/api/LoginApi.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Date;
 
 @RestController
-@CrossOrigin
 public class LoginApi {
 
     private UserManager userManager;
